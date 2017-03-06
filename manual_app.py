80a7b1713c7b895c800a96789094df097805193c
Akshay Budhkar <abudhkar@uwaterloo.ca>
2017-03-06 18:49:35 -0500

    Adds manual test

--- manual_app.py
+++ manual_app.py
@@ -1,35 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 18 16:13:51 2016
-
-@author: akshaybudhkar
-"""
+import numpy as np
 import os
 import sklearn
-from flask import Flask, render_template
-from flask_socketio import SocketIO
 from sklearn.externals import joblib
-import numpy as np
 import pandas as pd
 from scipy.fftpack import dct
-import tensorflow as tf
+
 
 def mad(data, axis=None):
     return np.mean(np.absolute(data - np.mean(data, axis)), axis)
 
+
 def feature_extraction(data, train=False):
     new_data = []
     print(data)
-    
+
     for f_data in data:
-        
+
         left_vals = np.array(f_data["values"]["left"])
         right_vals = np.array(f_data["values"]["right"])
-        
+
         a = left_vals
         b = right_vals
-        features = []            
-        
+        features = []
+
         # Left hand features
         if len(a) != 0 and len(a[0]) != 0:
             # Feature 1: Mean of DCT of Acceleration of X
@@ -81,140 +74,131 @@ def feature_extraction(data, train=False):
             # Feature 18/19: Standard Absolute Deviation and Mean of flex 5
             features.append(round(np.std(a[:, 10])))
             features.append(round(np.mean(a[:, 10])))
-        
+
         # Right hand features
         if len(b) != 0 and len(b[0]) != 0:
             # Feature 20: Mean of DCT of Acceleration of X
             transformed_values_x = np.array(dct(b[:, 0]))
             features.append(round(np.mean(transformed_values_x), 3))
-            
+
             # Feature 21: Mean of DCT of Acceleration of Y
             transformed_values_y = np.array(dct(b[:, 1]))
             features.append(round(np.mean(transformed_values_y), 3))
-            
+
             # Feature 22: Mean of DCT of Acceleration of Z
             transformed_values_z = np.array(dct(b[:, 2]))
             features.append(round(np.mean(transformed_values_z), 3))
-            
+
             # Feature 23/24: Mean Absolute Deviation and Mean of gyro in X
             features.append(round(mad(b[:, 3])))
             features.append(round(np.mean(b[:, 3])))
-            
+
             # Feature 25/26: Mean Absolute Deviation and Mean of gyro in Y
             features.append(round(mad(b[:, 4])))
             features.append(round(np.mean(b[:, 4])))
-            
+
             # Feature 27/28: Mean Absolute Deviation and Mean of gyro in Z
             features.append(round(mad(b[:, 5])))
             features.append(round(np.mean(b[:, 5])))
-            
+
             # Feature 29/30: Standard Absolute Deviation and Mean of flex 1
             features.append(round(np.std(b[:, 6])))
             features.append(round(np.mean(b[:, 6])))
-            
+
             # Feature 31/32: Standard Absolute Deviation and Mean of flex 2
             features.append(round(np.std(b[:, 7])))
             features.append(round(np.mean(b[:, 7])))
-            
+
             # Feature 33/34: Standard Absolute Deviation and Mean of flex 3
             features.append(round(np.std(b[:, 8])))
             features.append(round(np.mean(b[:, 8])))
-            
+
             # Feature 35/36: Standard Absolute Deviation and Mean of flex 4
             features.append(round(np.std(b[:, 9])))
             features.append(round(np.mean(b[:, 9])))
-            
+
             # Feature 37/38: Standard Absolute Deviation and Mean of flex 5
             features.append(round(np.std(b[:, 10])))
             features.append(round(np.mean(b[:, 10])))
-                
+
             if len(features) > 0:
                 if train:
                     new_data.append({"label": f_data["label"], "user": f_data["user"], "features": features[:26]})
                 else:
                     new_data.append({"features": features[:26]})
-    
+
     return new_data
 
 
 def process_data(data):
     final_data = []
-    
+
     values = {"left": [], "right": []}
 
-    print(data)
-    
     for point in data:
-        splits = point.split("|")
-        print "splitting"
-        print splits
-        left_array = splits[0].split(",")
-        right_array = splits[1].split(",")
-        
+
+        left_array = point.split(",")
+
         left_value = [float(left_array[0]), float(left_array[1]), float(left_array[2]),
                       float(left_array[3]), float(left_array[4]), float(left_array[5]),
                       float(left_array[6]), float(left_array[7]),
                       float(left_array[8]), float(left_array[9]), float(left_array[10])]
-                      
-        right_value = [float(right_array[0]), float(right_array[1]), float(right_array[2]),
-                      float(right_array[3]), float(right_array[4]), float(right_array[5]),
-                      float(right_array[6]), float(right_array[7]),
-                      float(right_array[8]), float(right_array[9]), float(right_array[10])]
-                      
+
+        right_value = [float(0) for x in range(12)]
+
         values["left"].append(left_value)
         values["right"].append(right_value)
-        
+
     final_data.append({"values": values})
-    
+
     predict_data = feature_extraction(final_data)
-    
+
     predict_df = pd.DataFrame(predict_data)
     X_pred = np.array(predict_df.features.tolist())
 
-    print predict_data
-    print X_pred
     print "Reached here"
     clf_1 = joblib.load('ml-models/svm_minus.pkl')
     preds_nb = clf_1.predict(X_pred)
 
     cols = joblib.load('ml-models/col_minus.pkl')
 
-    print(cols)
-    print(preds_nb)
+    print cols[preds_nb[0]]
+    return cols[preds_nb[0]]
 
-    return cols[preds_nb]
 
-build_data = []    
-app = Flask(__name__)
-sio = SocketIO(app)
+data = """7.81,-5.17,-5.17,0.03,0.03,0.03,474,417,432,333,379
+8.14,-4.93,-4.93,0.02,0.02,0.02,474,417,432,333,379
+7.47,-5.64,-5.64,-0.35,-0.35,-0.35,474,417,432,334,380
+4.96,-6.59,-6.59,-0.43,-0.43,-0.43,475,416,432,334,380
+4.96,-7.51,-7.51,-0.28,-0.28,-0.28,474,416,431,334,377
+1.49,-8.43,-8.43,-0.51,-0.51,-0.51,471,415,429,334,373
+-2.14,-8.74,-8.74,-0.26,-0.26,-0.26,468,412,428,334,372
+-1.46,-9.08,-9.08,0.16,0.16,0.16,467,412,430,334,377
+-1.80,-9.34,-9.34,0.02,0.02,0.02,467,412,430,334,377
+-2.02,-9.38,-9.38,0.04,0.04,0.04,466,411,431,334,378
+-1.64,-9.35,-9.35,0.01,0.01,0.01,466,411,432,334,379
+END
+-1.61,-9.32,-9.32,0.02,0.02,0.02,469,415,430,335,377
+7.77,-5.15,-5.15,0.04,0.04,0.04,469,415,430,335,377
+6.43,-5.54,-5.54,-0.18,-0.18,-0.18,469,415,430,336,377
+8.11,-5.81,-5.81,-0.30,-0.30,-0.30,469,414,430,336,378
+4.60,-7.42,-7.42,-0.46,-0.46,-0.46,467,413,429,335,379
+2.84,-7.93,-7.93,-0.26,-0.26,-0.26,465,412,428,334,376
+1.84,-9.53,-9.53,-0.34,-0.34,-0.34,461,409,428,334,375
+-1.08,-8.70,-8.70,-0.28,-0.28,-0.28,459,408,428,334,376
+-0.40,-9.75,-9.75,0.19,0.19,0.19,459,408,432,334,380
+-0.84,-9.29,-9.29,0.04,0.04,0.04,460,410,440,334,392
+-0.64,-9.28,-9.28,0.15,0.15,0.15,460,410,439,334,391
+END"""
 
-@app.route('/')
-def index():
-    """Serve the client-side application."""
-    return render_template('index.html')
+build_data = []
 
-@sio.on('connect')
-def connect():
-    print('A user has connected.')
+splits = data.split('\n')
 
-@sio.on('sendSensorData')
-def message(data):
-    global build_data   
-    splits = data.split('\n')
-    for line in splits:
-        if line != "END":
-            print 'Appended a line to the data.'
-            build_data.append(line)
-            sio.emit('receivedSensorData', data)
-        else:
-            value = process_data(build_data)
-            build_data = []
-            print 'Found END of the data.'
-            sio.emit('predictedValue', value)
-
-@sio.on('disconnect')
-def disconnect():
-    print('A user has disconnected')
-
-if __name__ == '__main__':
-    sio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+for line in splits:
+    if line != "END":
+        build_data.append(line)
+    else:
+        value = process_data(build_data)
+        build_data = []
+        print 'Found END of the data.'
